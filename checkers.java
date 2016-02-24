a3d582d48bcf8713fb115ce44f2a8cdb159c1518
Andrew Shepard <andrewshepard@outlook.com>
2016-02-24 11:23:42 -0500

    Update checkers.java

--- checkers.java
+++ checkers.java
@@ -1,20 +1,52 @@
-
-public class checkers {
+import java.util.*;
+public class chess {
 	public static void main(String[] args) {
-		String[][] mat = {	{"-","r","-","r","-","r","-","r", },
-							{"r","-","r","-","r","-","r","-", },
-							{"-","r","-","r","-","r","-","r", },
-							{"-","-","-","-","-","-","-","-", },
-							{"-","-","-","-","-","-","-","-", },
-							{"b","-","b","-","b","-","b","-", },
-							{"-","b","-","b","-","b","-","b", },
-							{"b","-","b","-","b","-","b","-", },
+		Scanner reader = new Scanner(System.in);
+		//board
+		String[][] mat = {	{"x","r","x","r","x","r","x","r", },
+							{"r","x","r","x","r","x","r","x", },
+							{"x","r","x","r","x","r","x","r", },
+							{"-","x","-","x","-","x","-","x", },
+							{"x","-","x","-","x","-","x","-", },
+							{"b","x","b","x","b","x","b","-", },
+							{"x","b","x","b","x","b","x","b", },
+							{"b","x","b","x","b","x","b","x", },
 								};
 		display(mat);
-		moveRed(mat, 1, 6, 2 , 5);
+		while(statusRed(mat) == true || statusBlack(mat) == true)
+		{	System.out.println(" ");
+			System.out.println("Enter first the piece you want to move in (row,column) and second where you would like to move it to in (row,column)");
+			int x1 = reader.nextInt();
+			int y1 = reader.nextInt();
+			int x = reader.nextInt();
+			int y = reader.nextInt();
+			move(mat, x1, y1, x , y);
+			display(mat);
+			System.out.println(" ");
+		}
+		move(mat, 3, 6, 2 , 5);
 		display(mat);
+		
 			}
 	
+public static boolean statusRed(String [][] mat)
+{	for (int i = 0; i < mat.length; i++) {
+		 for (int j = 0; j < mat[i].length; j++) {
+			 if (mat[i][j].equals("r"))
+				 return true;
+		 }
+	}
+return false;
+}
+public static boolean statusBlack(String [][] mat)
+{	for (int i = 0; i < mat.length; i++) {
+		 for (int j = 0; j < mat[i].length; j++) {
+			 if (mat[i][j].equals("b"))
+				 return true;
+		 }
+	}
+return false;
+}
 public static void display(String[][] mat){
 	//String alpha and numbers add the coordinate system for movement
 	String alpha = "12345678";
@@ -35,8 +67,15 @@ public static void display(String[][] mat){
 	}
 	
 }
-public static void moveRed(String[][] mat, int x, int y, int x1, int y1){
+public static void move(String[][] mat, int x1, int y1, int x, int y){
+	String z = mat[x1][y1];
+	if (z.equals("x"))
+		System.out.println("You cant move there!");
+	else if(z.equals("r") || z.equals("b"))
+		System.out.println("You cant move directly on a piece!");
+	else{
 	mat[x1][y1] = mat[x][y];
-	//this command doesnt work rn
+	mat[x][y] = z;
+	}
 }
 }
